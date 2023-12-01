eaf5f59c7a3dfa5236623da2739a798f3dc3ba6b
Anthony Moises Condori Choccata <magusslather@gmail.com>
2023-11-30 20:57:37 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+// Título
+const title = document.createElement('h1');
+title.textContent = 'Mi Aplicación';
+document.body.appendChild(title);
 
-export default App;
+// Cuadro de texto para la dirección URL
+const urlInput = document.createElement('input');
+urlInput.type = 'text';
+urlInput.placeholder = 'Ingrese una dirección URL';
+document.body.appendChild(urlInput);
+
+// Botón 1
+const button1 = document.createElement('button');
+button1.textContent = 'Botón 1';
+document.body.appendChild(button1);
+
+// Botón 2
+const button2 = document.createElement('button');
+button2.textContent = 'Botón 2';
+document.body.appendChild(button2);
